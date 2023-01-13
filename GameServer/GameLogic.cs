1928a025317a09191b7310099cbeaf3448883ed6
JinSjauw <jinzhouliu4@gmail.com>
2023-01-13 19:58:36 +0100

    Initial commit

---
+++ GameServer/GameLogic.cs
@@ -0,0 +1,17 @@
+﻿namespace GameServer;
+
+public class GameLogic
+{
+    public static void Update()
+    {
+        foreach (Client _client in Server.clients.Values)
+        {
+            if (_client.player != null)
+            {
+                _client.player.Update();
+            }
+        }
+        
+        ThreadManager.UpdateMain();
+    }
+}
